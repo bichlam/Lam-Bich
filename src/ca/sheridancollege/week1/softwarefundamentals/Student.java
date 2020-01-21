7824cf0e818a18e06619207ade6377291f5c481b
bichl <bichl@sheridancollege.ca>
2020-01-21 16:25:23 -0500

    fourth Commit

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -13,7 +13,8 @@ public class Student {
     
     private String name;
     private int student;
-
+    private String city;
+    private double year;
     /**
      * Get the value of name
      *
@@ -30,6 +31,48 @@ public class Student {
      */
     public void setName(String name) {
         this.name = name;
+    }
+
+    /**
+     * @return the student
+     */
+    public int getStudent() {
+        return student;
+    }
+
+    /**
+     * @param student the student to set
+     */
+    public void setStudent(int student) {
+        this.student = student;
+    }
+
+    /**
+     * @return the city
+     */
+    public String getCity() {
+        return city;
+    }
+
+    /**
+     * @param city the city to set
+     */
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    /**
+     * @return the year
+     */
+    public double getYear() {
+        return year;
+    }
+
+    /**
+     * @param year the year to set
+     */
+    public void setYear(double year) {
+        this.year = year;
     }
 
 }
